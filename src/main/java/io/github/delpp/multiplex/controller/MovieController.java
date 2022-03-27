da5625e237822021fcfbfe86b031ca84bdf27188
delpp <delpp@wp.pl>
2022-03-27 14:03:20 +0200

    Dodanie w kontrolerze metody do tworzenia nowego filmu/ obsługi "POST".

--- src/main/java/io/github/delpp/multiplex/controller/MovieController.java
+++ src/main/java/io/github/delpp/multiplex/controller/MovieController.java
@@ -5,6 +5,7 @@ import io.github.delpp.multiplex.model.MovieRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +15,6 @@ class MovieController {
 
     private final MovieRepository repository;
 
-
     public MovieController(MovieRepository repository) {
         this.repository = repository;
     }
@@ -22,6 +22,12 @@ class MovieController {
     @GetMapping(value = "/movies", params = {"!sort", "!page", "!size"})
     ResponseEntity<List<Movie>> realAllMovies(){
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @PostMapping("/movies")
+    ResponseEntity<Movie> createMovie(@RequestBody Movie movieToCreate){
+        repository.save(movieToCreate);
+        return ResponseEntity.noContent().build();
     }
 
 }
